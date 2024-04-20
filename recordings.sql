53e8c179054514448645168144cc499f558ea83f
Liza Mozolyuk <35519843+lizamozy@users.noreply.github.com>
2024-04-20 17:07:01 -0400

    Update recordings.sql

--- recordings.sql
+++ recordings.sql
@@ -6,3 +6,11 @@ id integer primary key autoincrement,
 rec_name varchar(50), 
 rec_path text
 );
+
+CREATE TABLE edited(
+id INTEGER PRIMARY KEY AUTOINCREMENT, 
+flag INTEGER, 
+edit_name varchar(50), 
+edit_path TEXT, 
+speed REAL
+);
