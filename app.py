d3057ca863bd6447bbe4dde3e2ee56acf33943f5
lizamozy <lizamozolyuk@gmail.com>
2024-04-23 22:09:55 -0400

    got record button to work, need to now show recordings

--- app.py
+++ app.py
@@ -13,25 +13,63 @@ import datetime
 #all important imports
     #other py files in this folder, simple audio, blah blah
 import thing_file
+import main
+import record
 # ======== Initialize flask app ===========
 app = Flask(__name__)
 
 
 @app.route('/'+thing_file.thing_name)
 def show_home():
+    main.setup()
     return render_template('main_page.html')
 
-
-#create main route
-    #record new audio
-    #view stored audio
-    #edit audio
-
 #record route
+@app.route('/'+thing_file.thing_name+'/record', methods=['POST'])
+def start_record():
     #api call to press button to record sound
     #need to signal the global vairable to chnage recording=True
+    try:
+        main.loop()
+    except KeyboardInterrupt:  
+        main.destroy()
+    result = main.record()
+    render_template('main_page.html', result=result)
+
 #route to see recordings
-    #database call to display all of recordings stored in orginal and edited database
+@app.route('/'+thing_file.thing_name+'/view-original')
+def view_original():
+    conn = record.connection("Recordings.db")
+    with conn:
+        sql = '''Select * from original'''
+        cur = conn.cursor()
+        # Execute the select statement
+        cur.execute(sql)
+        # Fetch all the rows from the result set
+        recordings = cur.fetchall()
+        print(recordings)
+    conn.close()
+    render_template('recordings.html', recordings=recordings)
+    
+    
+@app.route('/'+thing_file.thing_name+'/view-edited')
+def view_edited():
+    conn = record.connection("Recordings.db")
+    with conn:
+        sql = '''Select * from edited'''
+        cur = conn.cursor()
+        # Execute the select statement
+        cur.execute(sql)
+        # Fetch all the rows from the result set
+        recordings = cur.fetchall()
+        print(recordings)
+    conn.close()
+    render_template('recordings.html', recordings=recordings)
+    
+
+    
+
+   
     #next to orginal, have button that says edit
         #goes to edit route
 
@@ -48,6 +86,11 @@ def show_home():
     #play_wav(file_path)
 
 #route to edit
+@app.route('/'+thing_file.thing_name+'/edit')
+def edit_sound():
+    #what funcitons do i call to set up the edit audio??? talia needs to clarify
+    main.rotaryDeal()
+    
 
 #speed up
 
@@ -60,5 +103,6 @@ if __name__ == '__main__':
     # chmod -x flask_api_server.py
     #
     # Flask GitHub Issue: https://github.com/pallets/flask/issues/3189
-
     app.run(host="0.0.0.0", port=8080, debug=True) 
+    
+   
